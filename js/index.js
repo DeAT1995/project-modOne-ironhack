32e6759b7a00d97512c065ec93fa00bf6d33715d
DeAT1995 <debora2013aguiartavares@gmail.com>
2021-06-25 18:28:04 -0300

    move the emu and limit created, need to put at the canvas the obstacles, have some problem with the inserction

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 
 //A SER FEITO
 
-//começar o jogo
-//inserir player na tela
-// criar metodo para pular o obstaculo
-//criar metodo para retornar ao chão após pular o obstaculo
+//começar o jogo -- ok
+//inserir player na tela -- ok
+// criar obstaculos aleatórios na tela
 //criar o obstaculo 
 //definir velocidade dos obstaculos
 
@@ -16,52 +15,43 @@
 //redigir orientações sobre o jogo no READ.me
 
 
-
-/*window.onload = () => {
-
-    function startGame(){
-        player.draw();
-    }
-
-    let canvas = document.getElementById('canvas');
-    let context = canvas.getContext("2d");
-
-
-    class emu {
-        constructor(source, x, y , w, h){
-            this.posX = x;
-            this.posY = y;
-            this.width = w;
-            this.height = h;
-
-            const img = new Image();
-            img.src = source;
-            img.onload = () => {
-                this.img = img;
-            }
-        }
-    draw() {
-        context.drawImage(this.img, this.posX, this.posY, this.width, this.height);
-    }
-    }
-    const player = new Emu('/images/emu-icon edited.png', 200,100, 30, 30); 
-}
-*/ 
-
-
 window.onload = () => {
     document.getElementById("start-game").onclick = () => {
       startGame();
     };
 
+    document.addEventListener("keydown", (event)=>{
+        switch(event.key){
+            case "ArrowDown":
+                player.moveDown();
+                break;
+            case "ArrowUp":
+                player.moveUp();
+                break;
+        }
+    })
+
+
     function startGame() {
-        console.log(player);
         player.draw();
+        updateCanvas();
+        obstacle.draw();
         
     }
 
     const canvas = document.getElementById("game-box");
     const ctx = canvas.getContext('2d');
+
+    function updateCanvas(){
+       clearCanvas();
+        //verificar sobre o background , pois no exercicio da aula ele desenha o background , mas aqui eu nao tenho imagem predefinida de background
+        player.draw();
+        animationId = requestAnimationFrame(updateCanvas);
+    }
+
+    function clearCanvas(){
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+    }
 
     class Emu {
         constructor(source, x, y, w, h){
@@ -69,6 +59,8 @@ window.onload = () => {
             this.posY= y;
             this.width= w;
             this.height=h;
+            this.speed = 15;
+//adicionei o speed ,velocidade do emu na tela 
 
             const img = new Image();
             img.src = source;
@@ -80,11 +72,39 @@ window.onload = () => {
         draw(){
             ctx.drawImage(this.img,this.posX,this.posY,this.width,this.height);
         }
+        moveDown(){
+            if (this.posY > 10){
+            this.posY -= this.speed;
+            }
+        }
+        moveUp() {
+            if (this.posY < 280) {
+            this.posY += this.speed;
+        }
+        }
     }
-    const player = new Emu('./images/emu.png' , 20,142,115,115);
+    const player = new Emu('./images/emu.png' , 15,142,110,110);
+/*
+    class Obstacle {
+        constructor(source,x, y, w, h){
+            this.posX = x;
+            this.posY = y;
+            this.width = w;
+            this.height = h;
+            //this.speed =; //definir velocidade
+        }
+        draw(){
+            ctx.drawImage(this.img,this.posX,this.posY,this.width,this.height);
+        
+    }
+
+    const obstacle = new Obstacle('./images/pill-game.png', 100, 100, 50, 50);*/
 }
 
+
+
 //funcções que provavelmente existirão
-//function clearCanvas(){}
+//function clearCanvas(){} -- ok
+//as teclas estão ao contrario, arrow up, ta levando pra baixo e arrow down pra cima 
+//criar limite para o emu nao passar a tela -- ok
 //updateCanvas(){}
-//jump (){}
