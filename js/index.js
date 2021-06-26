85724aaa0dd0d8e87c0a14a5e642ae612b6fc24b
DeAT1995 <debora2013aguiartavares@gmail.com>
2021-06-26 15:29:16 -0300

    add obstacles at the game, created a randow for the obstacles, need to separete the itens of the array obstacles because they are comming all together

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 
 //começar o jogo -- ok
 //inserir player na tela -- ok
-// criar obstaculos aleatórios na tela
 //criar o obstaculo 
+// criar obstaculos aleatórios na tela
 //definir velocidade dos obstaculos
 
 //rodar um tempo de cronometro
@@ -22,10 +22,10 @@ window.onload = () => {
 
     document.addEventListener("keydown", (event)=>{
         switch(event.key){
-            case "ArrowDown":
+            case "ArrowUp":
                 player.moveDown();
                 break;
-            case "ArrowUp":
+            case "ArrowDown":
                 player.moveUp();
                 break;
         }
@@ -35,17 +35,26 @@ window.onload = () => {
     function startGame() {
         player.draw();
         updateCanvas();
-        obstacle.draw();
-        
+       // createObstacle.draw();
+       //obstacleCloroquina.draw();
+        //obstacleVirus.draw(); 
+        updateObstacles();
+
     }
 
     const canvas = document.getElementById("game-box");
     const ctx = canvas.getContext('2d');
+    let frames = 0;
+
 
     function updateCanvas(){
+        frames += 1;
        clearCanvas();
-        //verificar sobre o background , pois no exercicio da aula ele desenha o background , mas aqui eu nao tenho imagem predefinida de background
         player.draw();
+        //obstacleCloroquina.draw();
+        //obstacleVirus.draw();
+        updateObstacles();
+
         animationId = requestAnimationFrame(updateCanvas);
     }
 
@@ -99,12 +108,85 @@ window.onload = () => {
     }
 
     const obstacle = new Obstacle('./images/pill-game.png', 100, 100, 50, 50);*/
+
+    /*class Obstacle {
+        constructor(x){
+            this.x = 900;
+            this.y = y;
+            this.width = w;
+            this.height = h;
+        }
+
+        createObstacle(){
+            this.imgPill = new Image();
+            this.imgPill.src = './images/pill-game.png';
+            context.drawImage(this.imgPill, this.x, this.y, this.width, this.height);
+        }
+
+
+    }*/
+
+
+    class Obstacle {
+        constructor(source, x, y, w, h){
+            this.posX= x;
+            this.posY= y;
+            this.width= w;
+            this.height=h;
+            this.speed = 10;
+
+            const img = new Image();
+            img.src = source;
+            img.onload =() => {
+            this.img = img;
+            }
+        }
+
+        draw(){
+            ctx.drawImage(this.img,this.posX,this.posY,this.width,this.height);
+        }
+        
+        move(){
+            //como passar mais de uma coordenada (x e y) para o obstacle ??
+            this.posX -= this.speed;
+        }
+    }
+
+    const obstacles = [];
+
+    const obstacleCloroquina = new Obstacle('./images/medicini-icon-pill.png' , 900, 0, 100, 100);
+    const obstacleVirus = new Obstacle('./images/virus-icon.png' , 900, 200, 50, 50);
+    //const obstacleVacina = new Obstacle('./images/vaccine-icon.png' , 700, 200, 100, 100);
+
+    function createObstacle(){
+        const eixoX = 1000;//final do canvas
+        let eixoY = Math.floor(Math.random() * 400); //variável, número aleatorio multiplicado pelo tamanho do canvas no eixo y
+
+        obstacles.push(new Obstacle('./images/medicini-icon-pill.png' , eixoX, eixoY, 100, 100)); // ver como criar para mais de um obstaculo, pois na atividade do car ele tinha apenas um tipo de obstaculo
+        obstacles.push(new Obstacle('./images/virus-icon.png' , eixoX, eixoY, 50, 50));
+        obstacles.push(new Obstacle('./images/vaccine-icon.png' , eixoX, eixoY, 100, 100));    
+    }
+
+    function updateObstacles(){
+        obstacles.forEach((obstacle) =>{
+            console.log(obstacle)
+        obstacle.move();
+        obstacle.draw([0]);
+        })
+
+        if (frames % 80 === 0){ //parece que mesmo eu editando o valor continua a mesma velocidade
+            createObstacle();
+        }
+    }
 }
 
 
 
 //funcções que provavelmente existirão
 //function clearCanvas(){} -- ok
-//as teclas estão ao contrario, arrow up, ta levando pra baixo e arrow down pra cima 
+//as teclas estão ao contrario, arrow up, ta levando pra baixo e arrow down pra cima  --OK
 //criar limite para o emu nao passar a tela -- ok
-//updateCanvas(){}
+//updateCanvas(){} -- OK
+//colidir com um obstaculo
+//pontuação de acordo com o obstaculo
+//score do jogo
