4aadd3293b29c9cc1671e54fee1cbb71b71dddc0
Deat <debora2013aguiartavares@gmail.com>
2021-07-03 19:51:48 -0300

    comments removed, index.js organized

--- js/index.js
+++ js/index.js
@@ -1,49 +1,10 @@
-//A SER FEITO
-
-//começar o jogo -- ok
-//inserir player na tela -- ok
-//criar o obstaculo --ok
-// criar obstaculos aleatórios na tela -- ok
-//definir velocidade dos obstaculos --ok
-
-//arrumar os obstaculos --ok
-//fazer um score --ok
-//vacina soma ponto --------------
-//fazer a img da vacina sumir quando bater na ema --ok
-//cloroquina GAME OVER --ok
-//restart criado --ok
-//rodar um score --ok
-//fazer o score aparecer a pontuação do player ----------
-//fazer a tela voltar do inicio no restart -------------
-
-//adicionar um evento de mouse quando passar em cima dos li's para dar info sobre o jogo
-//redigir orientações sobre o jogo no README.md
-//fazer a apresentação em slides do jogo
-
-//funcções que provavelmente existirão
-//function clearCanvas(){} -- ok
-//as teclas estão ao contrario, arrow up, ta levando pra baixo e arrow down pra cima  --OK
-//criar limite para o emu nao passar a tela -- ok
-//updateCanvas(){} -- OK
-//colidir com um obstaculo -- ok
-//pontuação de acordo com o obstaculo vacina ---ok
-//score do jogo --ok
-//game over -- ok
-
-//coloquei a vacina pra sumir quando colide com o player --ok
-//criei o game over --ok
-//coloquei o refresh ---ok
-//averifguar o pq as vezes buga e some mais de uma vacina as vezes --ok
-// transformar o botão do start game em restart :D --ok
-// verificar como faço para a tela reinicar quando clicar no restart --ok
-// fazer as informações do jogo aoarecerem quando passar o mouse em cima --ok
-//função para fazer um texto ser exibido quando o mouse entrar nos links do navbar --ok
-
+// starts canvas access inside HTML
 window.onload = () => {
   document.getElementById("start-game").onclick = () => {
     startGame();
   };
 
+  //allows movement with the arrows
   document.addEventListener("keydown", (event) => {
     switch (event.key) {
       case "ArrowUp":
@@ -55,13 +16,26 @@ window.onload = () => {
     }
   });
 
+  
+  const canvas = document.getElementById("game-box");
+  const ctx = canvas.getContext("2d");
+  let frames = 0;
+  let animationId = null;
+  let obstacleVacina = [];
+  let obstacles = [];
+  let points = 0;
+
+
+
+  //starts the game
   function startGame() {
     player.draw();
     updateCanvas();
     updateObstacles();
     restartGame();
-  }
+  };
 
+  //reset the game
   function restartGame() {
     let hideButton = document.getElementById("start-game");
     hideButton.innerHTML = "RESTART";
@@ -73,18 +47,10 @@ window.onload = () => {
       player.draw();
       updateCanvas();
       updateObstacles();
-      // //fazer uma função para o botão restart
-
-      console.log(obstacles);
-      console.log(obstacleVacina);
     });
-  }
+  };
 
-  const canvas = document.getElementById("game-box");
-  const ctx = canvas.getContext("2d");
-  let frames = 0;
-  let animationId = null;
-
+  //update the canvas tag
   function updateCanvas() {
     frames += 1;
     clearCanvas();
@@ -94,12 +60,14 @@ window.onload = () => {
     animationId = requestAnimationFrame(updateCanvas);
     checkGameOver();
     takeVacine();
-  }
+  };
 
+  //clean the canvas tag
   function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  };
 
+  //create the player
   class Emu {
     constructor(source, x, y, w, h) {
       this.posX = x;
@@ -150,16 +118,19 @@ window.onload = () => {
         this.left() > obstacle.right()
       );
     }
-  }
+  };
+
+  //draw the player
   const player = new Emu("./images/emu.png", 15, 142, 90, 90);
 
+  //create the obstacles
   class Obstacle {
     constructor(source, x, y, w, h) {
       this.posX = x;
       this.posY = y;
       this.width = w;
       this.height = h;
-      this.speed = 2;
+      this.speed = 3;
 
       const img = new Image();
       img.src = source;
@@ -188,16 +159,13 @@ window.onload = () => {
     right() {
       return this.posX + this.width;
     }
-  }
+  };
 
-  let obstacleVacina = [];
-  let obstacles = [];
-
+  //create the obstacles
   function createObstacle() {
-    const eixoX = 1000; //final do canvas
-    let eixoY = Math.floor(Math.random() * 300); //variável, número aleatorio multiplicado pelo tamanho do canvas no eixo y
-
-    let numberVar = Math.floor(Math.random() * 400); // variável para criar um número aleatório para poder revesar os obstaculos a serem apresnetados na tela
+    const eixoX = 1000; 
+    let eixoY = Math.floor(Math.random() * 300);
+    let numberVar = Math.floor(Math.random() * 400);
 
     if (numberVar % 2) {
       return obstacleVacina.push(
@@ -208,8 +176,9 @@ window.onload = () => {
         new Obstacle("./images/cloroquina-trans.png", eixoX, eixoY, 50, 80)
       );
     }
-  }
+  };
 
+  //draw the obstacles
   function updateObstacles() {
     obstacles.forEach((obstacle) => {
       obstacle.move();
@@ -222,23 +191,21 @@ window.onload = () => {
     if (frames % 150 === 0) {
       createObstacle();
     }
-  }
+  };
 
-  let points = 0;
-
+  //create the player collision with the vaccine
   function takeVacine() {
     for (let i = 0; i < obstacleVacina.length; i += 1) {
-      // loop para passar por cada indice do array de vacina
-      let taken = player.crashWith(obstacleVacina[i]); // cofnere a colisao com o obstaculo
+      let taken = player.crashWith(obstacleVacina[i]); 
 
       if (taken) {
-        // se colidiu ele vai somar o points e remover o determinado obstaculo que colidiu
         points += 1;
         obstacleVacina.splice(i, 1);
       }
     }
-  }
+  };
 
+  //draw at canvas the score
   function score() {
     ctx.beginPath();
     ctx.fillStyle = "rgb(0, 0, 0)";
@@ -247,8 +214,9 @@ window.onload = () => {
     ctx.font = "15px serif";
     ctx.fillStyle = "rgba(0, 255, 33, 0.6)";
     ctx.fillText("SCORE: " + points, 455, 343); //como faço pra definir esse points como o score do game ???
-  }
+  };
 
+  //create the game over
   class GameOver {
     constructor(source, x, y, w, h) {
       this.posX = x;
@@ -263,8 +231,9 @@ window.onload = () => {
     draw() {
       ctx.drawImage(this.img, this.posX, this.posY, this.width, this.height);
     }
-  }
+  };
 
+  //draw the game over image
   const gameOverImg = new GameOver(
     "../images/game-over-trans.png",
     400,
@@ -273,6 +242,7 @@ window.onload = () => {
     200
   );
 
+  //check if are the player collision with the cloroquina obstacle
   function checkGameOver() {
     const crashed = obstacles.some(function (obstacle) {
       return player.crashWith(obstacle);
